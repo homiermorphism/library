d1dd005c53eb60fcbcca850759fbfb9a047b124a
Samantha <samanthahomier@yahoo.com>
2020-01-30 21:00:16 -0800

    added addBookToLibrary function; render function; no styling done whatsoever

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-function book(title, author, year, pages, status) {
+const bigDiv = document.querySelector('.big-div');
+
+let myLibrary = [
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    year: '1937',
+    status: 'read'
+  },
+  {
+    title: 'The Martian',
+    author: 'Andy Weir',
+    year: '2011',
+    status: 'read'
+  }
+]
+
+function book(title, author, year, status) {
   this.title = title;
   this.author = author;
   this.year = year;
-  this.pages = pages;
   this.status = status;
+  }
 
-  this.info = function() {
-    return this.title + ' by ' + this.author + ', written in ' + this.year +
-      ', ' + this.pages + ' pages, ' + this.status;
+
+function addBookToLibrary(title, author, year, status) {
+  let newBook = new book(title, author, year, status);
+  myLibrary.push(newBook);
+}
+
+function render() {
+  for (i=0; i < myLibrary.length; i++) {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    bigDiv.appendChild(card);
+
+    let cardTitle = document.createElement('div');
+    let cardAuthor = document.createElement('div');
+    let cardYear = document.createElement('div');
+    let cardStatus = document.createElement('div');
+
+    cardTitle.classList.add('title');
+    cardAuthor.classList.add('author');
+    cardYear.classList.add('year');
+    cardStatus.classList.add('status');
+
+    cardTitle.innerHTML = myLibrary[i].title;
+    cardAuthor.innerHTML = myLibrary[i].author;
+    cardYear.innerHTML = myLibrary[i].year;
+    cardStatus.innerHTML = myLibrary[i].status;
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardYear);
+    card.appendChild(cardStatus);
+
   }
 }
 
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', '1937', '295', 'read');
-
-console.log(theHobbit.info());
+render();
