3629a354604bf9ccb5994510acbcb2afb04dcd14
Samantha <samanthahomier@yahoo.com>
2020-02-02 22:53:10 -0800

    added read/unread button; added delete button, but it doesn't work

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const formYear = document.getElementById('year');
 const formGenre = document.getElementById('genre');
 const formStatus = document.getElementById('status');
 
+
 let loadedLibrary = [];
 let myLibrary = [
   {
@@ -51,6 +52,35 @@ let myLibrary = [
   }
 ]
 
+render();
+
+// need to load these after the cards have been rendered
+let delButtons = document.querySelectorAll('.del-btn');
+let statusButtons = document.querySelectorAll('.status-btn');
+
+
+newBookButton.addEventListener('click', showForm);
+formBackground.addEventListener('click', hideForm);
+cancelButton.addEventListener('click', hideForm);
+submitButton.addEventListener('click', submit);
+
+for (i=0; i < statusButtons.length - 1; i++) {
+  statusButtons[i].addEventListener('click', function(e) {
+    if (e.target.value === 'read') {
+      e.target.value = 'unread';
+      e.target.innerHTML = 'Unread';
+      e.target.classList.toggle('unread');
+      e.target.classList.toggle('read');
+    }
+    else if (e.target.value === 'unread') {
+      e.target.value = 'read';
+      e.target.innerHTML = 'Read';
+      e.target.classList.toggle('unread');
+      e.target.classList.toggle('read');
+    }
+  });
+}
+
 function book(title, author, year, genre, status) {
   this.title = title;
   this.author = author;
@@ -58,11 +88,6 @@ function book(title, author, year, genre, status) {
   this.genre = genre;
   this.status = status;
 }
-
-newBookButton.addEventListener('click', showForm);
-formBackground.addEventListener('click', hideForm);
-cancelButton.addEventListener('click', hideForm);
-submitButton.addEventListener('click', submit);
 
 function showForm() {
   newBookForm.style.display = 'block';
@@ -94,6 +119,14 @@ function submit() {
   render();
 }
 
+function deleteCard(e) {
+  for (i=0; i < myLibrary.length; i++) {
+    if (e.target.getAttribute('data-id') === i) {
+      console.log('hi');
+    }
+  }
+}
+
 function newBookToLibrary(title, author, year, genre, status) {
   let newBook = new book(title, author, year, genre, status);
   myLibrary.push(newBook);
@@ -107,6 +140,7 @@ function render() {
     else {
       let card = document.createElement('div');
       card.classList.add('card', 'medium');
+      card.setAttribute('data-id', i);
       cardsDiv.appendChild(card);
 
       let cardContent = document.createElement('div');
@@ -124,23 +158,43 @@ function render() {
       cardContent.appendChild(cardAuthor);
 
       let img = document.createElement('img');
-      img.classList.add('activator', 'z-depth-4');
+      img.classList.add('activator', 'z-depth-3');
       if (myLibrary[i].title === 'The Hobbit') {
         img.src = 'images/hobbit.jpg';
       }
       else if (myLibrary[i].title === 'The Martian') {
         img.src = 'images/themartian.jpg';
       }
+      else {
+        img.src = 'images/nocover.jpg';
+      }
       cardContent.appendChild(img);
 
       let cardInfo = document.createElement('p');
       cardInfo.innerHTML = myLibrary[i].year +
-        '<br>' + myLibrary[i].genre + '<br>' + myLibrary[i].status;
+        '<br>' + myLibrary[i].genre;
       cardContent.appendChild(cardInfo);
+
+      let statusButton = document.createElement('button');
+      statusButton.classList.add('btn', 'status-btn');
+      statusButton.value = myLibrary[i].status;
+      if (statusButton.value === 'read') {
+        statusButton.innerHTML = 'Read';
+        statusButton.classList.add('read');
+      }
+      else if (statusButton.value === 'unread') {
+        statusButton.innerHTML = 'Unread';
+        statusButton.classList.add('unread');
+      }
+      cardContent.appendChild(statusButton);
+
+      let delButton = document.createElement('a');
+      delButton.classList.add('btn-floating', 'del-btn');
+      delButton.setAttribute('data-id', i);
+      delButton.innerHTML = '<i class="material-icons">delete</i>';
+      card.appendChild(delButton);
 
       loadedLibrary.push(myLibrary[i]);
     }
   }
 }
-
-render();
